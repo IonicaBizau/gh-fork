a510f968a474bd6a9e5d2373538647b30bbef4b5
Ionică Bizău <bizauionica@gmail.com>
2016-04-03 17:52:52 +0300

    :memo:

--- example/index.js
+++ example/index.js
@@ -1,6 +1,7 @@
 const ghFork = require("../lib");
 
-ghFork("jillix", "jxoss", "65494e1cd47b9278dc9454b004c58267f1f42df1", function (err, data) {
+// Fork everything from @jillix in @jxoss.
+ghFork("jillix", "jxoss", "your token", function (err, data) {
     console.log(err || "Done");
 }).on("repo-success", data => {
     console.log(`Forked ${data.source.full_name} into ${data.full_name}`);
